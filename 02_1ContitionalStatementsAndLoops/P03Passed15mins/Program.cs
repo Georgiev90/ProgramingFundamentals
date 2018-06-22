832b441840b7edfb19a1ee553da44985e416804a
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-06-22 14:53:57 +0300

    Add files via upload

---
+++ 02_1ContitionalStatementsAndLoops/P03Passed15mins/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P03Passed15mins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int hours = int.Parse(Console.ReadLine());
+			int minutes = int.Parse(Console.ReadLine());
+			minutes += 30;
+
+			if (minutes > 59)
+			{
+				minutes -= 60;
+				hours++;
+				if (hours == 24)
+				{
+					hours = 0;
+				}
+			}
+			Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
---
+++ 02_1ContitionalStatementsAndLoops/P05ForeignLanguages/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P05ForeignLanguages
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string country = Console.ReadLine();
+			switch (country)
+			{
+				case "USA":
+
+				case "Canada":
+
+				case "England":
+					Console.WriteLine("English");
+					break;
+
+				case "Spain":
+
+				case "Argentina":
+
+				case "Mexico": Console.WriteLine("Spanish");
+					break;
+
+				case "Portugal":
+
+				case "Brasil": Console.WriteLine("Portuguese");
+					break;
+
+				default:
+					Console.WriteLine("unknown");
+					break;
+
+				 
+			}
+		}
+	}
+}
