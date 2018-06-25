4c66a7bc25e52320031fdd51d9c21b4f9fa3e34b
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-06-25 11:57:33 +0300

    Add files via upload

---
+++ 02ContitionalStatementsAndLoops/03RestaurantDiscount/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace RestaurantDiscount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupSize = int.Parse(Console.ReadLine());
+            string package = Console.ReadLine();
+            int hallPrice = 0;
+            string hallName = "";
+            double discount = 0.0;
+            int packagePrice = 0;
+            if (groupSize <= 50)
+            {
+                hallPrice = 2500;
+                hallName = "Small Hall";
+            }
+            else if (groupSize <= 100)
+            {
+                hallPrice = 5000;
+                hallName = "Terrace";
+            }
+            else if (groupSize <= 120)
+            {
+                hallPrice = 7500;
+                hallName = "Great Hall";
+            }
+            else
+            {
+                Console.WriteLine("We do not have an appropriate hall.");
+                return;
+            }
+            if ("Normal".Equals(package))
+            {
+                packagePrice = 500;
+                discount = 5;
+            }
+            else if ("Gold".Equals(package))
+            {
+                packagePrice = 750;
+                discount = 10;
+            }
+
+            else
+            {
+                packagePrice = 1000;
+                discount = 15;
+            }
+            double totalDiscount = discount * (hallPrice + packagePrice) / 100.0;
+            double totalPrice = hallPrice + packagePrice-totalDiscount;
+            double pricePerPerson = totalPrice / groupSize;
+           Console.WriteLine($"We can offer you the {hallName}\r\nThe price per person is {pricePerPerson:F2}$");
+        }
+    }
+}
+
---
+++ 02ContitionalStatementsAndLoops/04Hotel/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _04Hotel
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string month = Console.ReadLine();
+			int nightsCount = int.Parse(Console.ReadLine());
+			double studio = 0.0;
+			double Double = 0.0;
+			double suite = 0.0;
+
+
+			if ("May" == month || "October" == month)
+			{
+
+				studio = 50;
+				Double = 65;
+				suite = 75;
+
+				if (nightsCount > 7)
+				{
+					studio *= 0.95;
+
+				}
+
+			}
+			else if ("June" == month || "September" == month)
+			{
+				studio = 60;
+				Double = 72;
+				suite = 82;
+				if (nightsCount > 14)
+				{
+					Double *= 0.9;
+				}
+
+			}
+			else if ("July" == month || "August" == month || "December" == month)
+			{
+				studio = 68;
+				Double = 77;
+				suite = 89;
+				if (nightsCount > 14)
+				{
+					suite *= 0.85;
+				}
+			}
+
+			double endPriceStudio = studio * nightsCount;
+			double endPriceDouble = Double * nightsCount;
+			double endPriceSuite = suite * nightsCount;
+
+			if ((month == "September" || month == "October") && nightsCount > 7)
+			{
+				endPriceStudio -= studio;
+			}
+
+			Console.WriteLine($"Studio: {endPriceStudio:f2} lv.");
+			Console.WriteLine($"Double: {endPriceDouble:f2} lv.");
+			Console.WriteLine($"Suite: {endPriceSuite:f2} lv.");
+
+
+
+
+		}
+	}
+}
