76a3d544309f2705ab65aced5fb0d57789d47d5f
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-06-25 11:49:47 +0300

    Add files via upload

---
+++ 02ContitionalStatementsAndLoops/01ChooseAdrink/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ChooseAdrink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            string drink = "";
+        
+
+            if ("Athlete".Equals(profession))
+            {
+                drink = "Water";
+            }
+            else if ("Businessman".Equals(profession) ||
+               "Businesswoman".Equals(profession))
+            {
+                drink = "Coffee";
+
+            }
+            else if ("SoftUni Student".Equals(profession))
+            {
+                drink = "Beer";
+            }
+            else
+            {
+                drink = "Tea";
+            }
+
+
+            Console.WriteLine(drink);
+        }
+    }
+}
+
---
+++ 02ContitionalStatementsAndLoops/02ChooseAdrinkV2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ChooseAdrink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            string drink = "";
+            double price = 0.0;
+
+
+
+            if ("Athlete".Equals(profession))
+            {
+                drink = "Water";
+                price = 0.7;
+
+            }
+            else if ("Businessman".Equals(profession) ||
+               "Businesswoman".Equals(profession))
+            {
+                drink = "Coffee";
+                price = 1.0;
+
+            }
+            else if ("SoftUni Student".Equals(profession))
+            {
+                drink = "Beer";
+                price = 1.7;
+            }
+            else
+            {
+                drink = "Tea";
+                price = 1.2;
+            }
+            double totalPrice = price * quantity;
+            
+
+
+            Console.WriteLine($"The {profession} has to pay {totalPrice:f2}.");
+        }
+    }
+}
+
