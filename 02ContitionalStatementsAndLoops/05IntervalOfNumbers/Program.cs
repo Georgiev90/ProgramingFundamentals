f6c525a9d367110ecede7c443abae076808fb771
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-06-27 21:29:07 +0300

    Add files via upload

---
+++ 02ContitionalStatementsAndLoops/05IntervalOfNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05IntervalOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int startNum = int.Parse(Console.ReadLine());
+			int endNum = int.Parse(Console.ReadLine());
+			if (startNum > endNum) {  
+				int max = startNum;
+				startNum = endNum;
+				endNum = max;
+			}
+			while (startNum <= endNum)
+			{
+				Console.WriteLine(startNum);
+				startNum++;
+			}
+        }
+    }
+}
