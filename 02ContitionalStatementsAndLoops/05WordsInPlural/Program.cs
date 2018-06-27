8d036613afea08378beb40b2168a658d1ad700a1
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-06-27 21:30:06 +0300

    Add files via upload

---
+++ 02ContitionalStatementsAndLoops/05WordsInPlural/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05WordsInPlural
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string noun = Console.ReadLine();
+
+			if (noun.EndsWith('y'))
+			{
+				noun = noun.Remove(noun.Length - 1, 1) + "ies";
+			}
+			else if ((noun.EndsWith('o')) || noun.EndsWith('o') || noun.EndsWith('c')
+			   || noun.EndsWith('h') || noun.EndsWith('s')
+			   || noun.EndsWith("sh") || noun.EndsWith('x')
+			   || noun.EndsWith('z'))
+			{
+				noun += "es";
+			}
+			else
+			{
+				noun += "s";
+			}
+			Console.WriteLine(noun);
+		}
+
+	}
+}
+
+
+
