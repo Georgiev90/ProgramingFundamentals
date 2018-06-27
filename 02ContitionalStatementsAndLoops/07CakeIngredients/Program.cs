5675d42ec876804dcaec52ff2c43fa4f8621d4c5
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-06-27 21:31:02 +0300

    Add files via upload

---
+++ 02ContitionalStatementsAndLoops/07CakeIngredients/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07CakeIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string ingredient = "";
+			int count = 0;
+			bool adding = true;
+			while (adding) {
+				ingredient = Console.ReadLine();
+				if (ingredient == "Bake!")
+				{
+					adding = false;
+			
+				}
+				else
+				{
+					Console.WriteLine($"Adding ingredient {ingredient}.");
+					count++;
+
+				}
+				
+			}
+			Console.WriteLine($"Preparing cake with {count} ingredients.");
+        }
+    }
+}
