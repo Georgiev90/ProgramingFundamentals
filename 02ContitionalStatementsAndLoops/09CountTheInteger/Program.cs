11da8a5ba50efacd2f0d7b23490a8ae4cec63405
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-06-27 21:56:34 +0300

    Add files via upload

---
+++ 02ContitionalStatementsAndLoops/09CountTheInteger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09CountTheInteger
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int integerCount = 0;
+
+		
+			try
+			{
+				while (true) { 
+				int input = int.Parse(Console.ReadLine());
+				integerCount++;
+				}
+			}
+
+			catch (Exception)
+			{
+				Console.WriteLine(integerCount);
+			}
+		}
+	}
+}
+
+
+
