33c0afeef969da60648054300603e782395411f9
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-06-28 23:30:03 +0300

    Add files via upload

---
+++ 02ContitionalStatementsAndLoops/14MagicLetter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _14MagicLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			char startC = char.Parse(Console.ReadLine());
+			char endC = char.Parse(Console.ReadLine());
+			char excludedC = char.Parse(Console.ReadLine());
+			for (char c1 = startC; c1 <= endC; c1++)
+			{
+				for (char c2 = startC; c2 <= endC; c2++)
+				{
+					for (char c3 = startC; c3 <= endC; c3++)
+					{
+						if (c1 != excludedC && c2 != excludedC && c3 != excludedC)
+						{
+							Console.Write($"{c1}{c2}{c3} ");
+						}
+
+
+					}
+				}
+			}
+        }
+    }
+}
