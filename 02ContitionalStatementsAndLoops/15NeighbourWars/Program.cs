da8922efe551c6615c52cdeb1fe7c0631daa58a1
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-06-28 23:30:55 +0300

    Add files via upload

---
+++ 02ContitionalStatementsAndLoops/15NeighbourWars/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _15NeighbourWars
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int PeshoDmg = int.Parse(Console.ReadLine());
+			int GoshoDmg = int.Parse(Console.ReadLine());
+			int PeshoHp = 100;
+			int GoshoHp = 100;
+			int atackCount = 0;
+
+			while (true)
+			{
+				atackCount++;
+				if (atackCount % 2 != 0)
+				{
+					if (GoshoHp-PeshoDmg<=0)
+					{
+						Console.WriteLine($"Pesho won in {atackCount}th round.");
+						return;
+					}
+					else { 
+					GoshoHp -= PeshoDmg;
+					Console.WriteLine($"Pesho used Roundhouse kick and reduced Gosho to {GoshoHp} health.");
+					}
+				}
+				else if (atackCount % 2 == 0)
+				{
+					if (PeshoHp-GoshoDmg <= 0)
+					{
+						Console.WriteLine($"Gosho won in {atackCount}th round.");
+						return;
+					}
+					else { 
+					PeshoHp -= GoshoDmg;
+					Console.WriteLine($"Gosho used Thunderous fist and reduced Pesho to {PeshoHp} health.");
+					}
+				}
+				if (atackCount % 3 == 0)
+				{
+					GoshoHp += 10;
+					PeshoHp += 10;
+				}
+			}
+		}
+
+	}
+}
+
+
+
