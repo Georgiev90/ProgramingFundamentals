5cc8209f9ec681ed63f92321b414aeaf7209e0bb
Georgiev90 <37441343+Georgiev90@users.noreply.github.com>
2018-07-11 15:15:47 +0300

    Add files via upload

---
+++ 03DataTypesAndVariables/00LiveDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _00LiveDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int num = 0;
+			//b/b/b/b/d/d/e/c 
+			while (true)
+			{
+				
+				num = int.Parse(Console.ReadLine());
+				if (num % 2 == 0)
+				{
+					Console.WriteLine("Please write an odd number.");
+					
+				}
+				else
+				{
+					break;
+				}
+			}
+			Console.WriteLine($"The number is: {Math.Abs(num)}");
+        }
+    }
+}
